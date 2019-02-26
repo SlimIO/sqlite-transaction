27d03ecccb791d95f8a52ebdbff3e7afeb537e1d
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-02-26 11:31:57 +0100

    test: start writing test with japa

--- test/test.js
+++ test/test.js
@@ -1,6 +1,31 @@
-// Require Third-party Dependencies
-const avaTest = require("ava");
+// Require Node.js Dependencies
+const { join } = require("path");
+const { unlink } = require("fs").promises;
 
-avaTest("Husky passed Test", (assert) => {
-    assert.pass();
+// Require Third-party Dependencies
+const test = require("japa");
+const sqlite = require("sqlite");
+
+// Require Internal
+const TransactionManager = require("../");
+
+// CONSTANTS
+const DB = join(__dirname, "test.db");
+
+test.group("SQLite-transaction", (group) => {
+    /** @type {TransactionManager.Database} */
+    let db;
+
+    group.before(async() => {
+        db = await sqlite.open(DB);
+    });
+
+    group.after(async() => {
+        await db.close();
+        await unlink(DB);
+    });
+
+    test("Create a new TransactionManager", (assert) => {
+        assert.equal(true, true);
+    });
 });
